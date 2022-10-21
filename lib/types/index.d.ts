c07f6541125611d9341ece7fb9c2951497b0e2e2
Pepe Becker <mail@pepebecker.com>
2022-10-21 21:43:48 +0900

    Build module
    Signed-off-by: Pepe Becker <mail@pepebecker.com>

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,11 +1,19 @@
+import { LANGUAGE } from './languages';
 export interface Config {
     NAVER_CLIENT_ID: string;
     NAVER_CLIENT_SECRET: string;
+}
+interface TranslateOptions {
+    source?: LANGUAGE;
+    target?: LANGUAGE;
+    honorific?: boolean;
 }
 declare class Papago {
     config: Config;
     constructor(config: Config);
     isConfigured(): boolean;
-    translate(term: string, enko?: boolean): Promise<any>;
+    translate(text: string): Promise<string>;
+    translate(text: string, enko: boolean): Promise<string>;
+    translate(text: string, options: TranslateOptions): Promise<string>;
 }
 export default Papago;
