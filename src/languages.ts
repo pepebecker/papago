0df3c054613f9b775dcda071eac2667e5fc983a2
Pepe Becker <mail@pepebecker.com>
2022-10-21 21:30:59 +0900

    Add support for multiple languages
    Signed-off-by: Pepe Becker <mail@pepebecker.com>

---
+++ src/languages.ts
@@ -0,0 +1,34 @@
+export type KOREAN = 'ko';
+export type ENGLISH = 'en';
+export type JAPANESE = 'ja';
+export type CHINESE_SIMPLIFIED = 'zh-CN';
+export type CHINESE_TRADITIONAL = 'zh-TW';
+export type SPANISH = 'es';
+export type FRENCH = 'fr';
+export type GERMAN = 'de';
+export type RUSSIAN = 'ru';
+export type PORTUGUESE = 'pt';
+export type ITALIAN = 'it';
+export type VIETNAMESE = 'vi';
+export type THAI = 'th';
+export type INDONESIAN = 'id';
+export type HINDI = 'hi';
+export type LANGUAGE = KOREAN | ENGLISH | JAPANESE | CHINESE_SIMPLIFIED | CHINESE_TRADITIONAL | SPANISH | FRENCH | GERMAN | RUSSIAN | PORTUGUESE | ITALIAN | VIETNAMESE | THAI | INDONESIAN | HINDI;
+
+export const languages = {
+  ko: 'Korean',
+  en: 'English',
+  ja: 'Japanese',
+  'zh-CN': 'Chinese (Simplified)',
+  'zh-TW': 'Chinese (Traditional)',
+  es: 'Spanish',
+  fr: 'French',
+  de: 'German',
+  ru: 'Russian',
+  pt: 'Portuguese',
+  it: 'Italian',
+  vi: 'Vietnamese',
+  th: 'Thai',
+  id: 'Indonesian',
+  hi: 'Hindi',
+};
